d3a51ec9398920736159f78c9c008519692066ba
nvhoan <nvhoan911@gmail.com>
2022-11-24 20:15:19 +0700

    rename key localStogare

--- front-end/src/context/RequiredAuth/requiredAuth.js
+++ front-end/src/context/RequiredAuth/requiredAuth.js
@@ -1,10 +1,11 @@
 import { useAuthContext } from './authContext';
 import { Navigate } from 'react-router-dom';
+import { TOKEN_KEY } from '../../constants';
 
 function RequiredAuth(props) {
     const { children } = props;
     const { isAuthenticated } = useAuthContext();
-    return isAuthenticated || localStorage.getItem('localStorage') ? children : <Navigate to="/login" />;
+    return isAuthenticated || localStorage.getItem(TOKEN_KEY) ? children : <Navigate to="/login" />;
 }
 
 export default RequiredAuth;
