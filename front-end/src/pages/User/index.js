eeea9aee8b873f800e3a2476bb778c8ee8f2d48b
nvhoan <nvhoan911@gmail.com>
2022-11-25 21:17:23 +0700

    fix conflict

--- front-end/src/pages/User/index.js
+++ front-end/src/pages/User/index.js
@@ -14,30 +14,8 @@ import { TypeFilter } from './TypeFilter/TypeFilter';
 import styles from './User.module.scss';
 
 function User() {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-<<<<<<< HEAD
-    const [userDetails, setUserDetails] = useState('');
-    const [dataState, setDataState] = useState([]);
-
-    const handleClose = () => {
-        setShow(false);
-        setUserDetails('');
-    };
-    const handleShow = (staffCode) => {
-        setShow(true);
-        setUserDetails(dataState.find((c) => c.staffCode === staffCode));
-    };
-
-    const getData = async () => {
-        const data = await getAllData('User');
-        setDataState(data);
-    };
-
-    useEffect(() => {
-        getData();
-    }, []);
-=======
   const [userDetails, setUserDetails] = useState('');
   const [dataState, setDataState] = useState([]);
   const [searchValue, setSearchValue] = useState('');
@@ -72,100 +50,56 @@ function User() {
     // I want call a function when first render
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
->>>>>>> b1b7fa7f559fc04663b84b4bc68b8e922629df8e
 
-    const columns = [
-        {
-            name: 'Staff Code',
-            selector: (row) => row.staffCode,
-            sortable: true,
-        },
-        {
-            name: 'Full Name',
-            selector: (row) => row.fullName,
-            sortable: true,
-            cell: (row) => {
-                return <Link onClick={() => handleShow(row.staffCode)}>{row.fullName}</Link>;
-            },
-        },
-        {
-            name: 'Username',
-            selector: (row) => row.userName,
-        },
-        {
-            name: 'Joined Date',
-            selector: (row) => row.joinedDate,
-            sortable: true,
-            cell: (row) => {
-                return <div>{dateStrToStr(row.joinedDate)}</div>;
-            },
-        },
-        {
-            name: 'Type',
-            selector: (row) => row.type,
-            sortable: true,
-        },
-        {
-            name: 'Action',
-            selector: (row) => row.null,
-            cell: (row) => [
-                <Link key={row.staffCode} to={`#`} className={styles.customPen}>
-                    <FontAwesomeIcon icon={faPen} />
-                </Link>,
-                <Link
-                    key={`keyDelete_${row.staffCode}`}
-                    to={'#'}
-                    style={{ cursor: 'pointer', color: 'red', fontSize: '1.5em', marginLeft: '10px' }}
-                >
-                    <FontAwesomeIcon icon={faRemove} />
-                </Link>,
-            ],
-        },
-    ];
+  const columns = [
+    {
+      name: 'Staff Code',
+      selector: (row) => row.staffCode,
+      sortable: true,
+    },
+    {
+      name: 'Full Name',
+      selector: (row) => row.fullName,
+      sortable: true,
+      cell: (row) => {
+        return <Link onClick={() => handleShow(row.staffCode)}>{row.fullName}</Link>;
+      },
+    },
+    {
+      name: 'Username',
+      selector: (row) => row.userName,
+    },
+    {
+      name: 'Joined Date',
+      selector: (row) => row.joinedDate,
+      sortable: true,
+      cell: (row) => {
+        return <div>{dateStrToStr(row.joinedDate)}</div>;
+      },
+    },
+    {
+      name: 'Type',
+      selector: (row) => row.type,
+      sortable: true,
+    },
+    {
+      name: 'Action',
+      selector: (row) => row.null,
+      cell: (row) => [
+        <Link key={row.staffCode} to={`#`} className={styles.customPen}>
+          <FontAwesomeIcon icon={faPen} />
+        </Link>,
+        <Link
+          key={`keyDelete_${row.staffCode}`}
+          to={'#'}
+          style={{ cursor: 'pointer', color: 'red', fontSize: '1.5em', marginLeft: '10px' }}
+        >
+          <FontAwesomeIcon icon={faRemove} />
+        </Link>,
+      ],
+    },
+  ];
 
-<<<<<<< HEAD
-    const [searchValue, setSearchValue] = useState('');
-
-    const onSearch = async (value) => {
-        setSearchValue(value);
-        let data = await getAllData(`User`);
-        if (value) {
-            data = await getAllDataWithFilterBox(`User/query` + queryToString({ valueSearch: value }));
-        } else {
-            data = await getAllData(`User`);
-        }
-        setDataState(data);
-    };
-
-    return (
-        <div className="main tableMain">
-            <h1 className="tableTitle">User List</h1>
-            <div className="tableExtension">
-                <div className="tableExtensionLeft">
-                    <TypeFilter setDataState={setDataState} />
-                </div>
-
-                <div className="tableExtensionRight">
-                    <SearchUser onSearch={onSearch} searchValue={searchValue} />
-                    <ButtonCreate />
-                </div>
-            </div>
-
-            <DataTable
-                columns={columns}
-                data={dataState}
-                noHeader
-                defaultSortField="id"
-                defaultSortAsc={true}
-                pagination
-                highlightOnHover
-                dense
-            />
-
-            <ModalDetails userDetails={userDetails} handleClose={handleClose} show={show} />
-        </div>
-    );
-=======
   // Search
   const onSearch = async (value) => {
     setLoading(true);
@@ -355,7 +289,6 @@ function User() {
       <ModalDetails userDetails={userDetails} handleClose={handleClose} show={show} />
     </div>
   );
->>>>>>> b1b7fa7f559fc04663b84b4bc68b8e922629df8e
 }
 
 export default User;
