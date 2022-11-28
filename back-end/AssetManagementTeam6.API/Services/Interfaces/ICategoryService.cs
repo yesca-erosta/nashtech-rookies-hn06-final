87ecfc99ac4cc1c70e90cc532de5d6868c518f0a
nguyen ngoc tien <nguyentien9969@gmail.com>
2022-11-28 14:45:46 +0700

    asset, category

--- back-end/AssetManagementTeam6.API/Services/Interfaces/ICategoryService.cs
+++ back-end/AssetManagementTeam6.API/Services/Interfaces/ICategoryService.cs
@@ -6,7 +6,7 @@ namespace AssetManagementTeam6.API.Services.Interfaces
     public interface ICategoryService
     {
         Task<Category> GetCategoryById(string id);
-        Task<Category?> Create(Category createRequest);
+        Task<Category?> Create(CategoryRequest createRequest);
         Task<IEnumerable<Category>> GetAllAsync();
     }
 }
---
+++ back-end/AssetManagementTeam6.API/Validation/CategoryRequestValidation.cs
@@ -0,0 +1,24 @@
+﻿using AssetManagementTeam6.API.Dtos.Requests;
+using AssetManagementTeam6.Data.Entities;
+using Common.Constants;
+using FluentValidation;
+
+namespace AssetManagementTeam6.API.Validation
+{
+    public class CategoryRequestValidatior : AbstractValidator<CategoryRequest>
+    {
+        public CategoryRequestValidatior()
+        {
+
+            RuleFor(x => x.Id)
+             .NotNull()
+             .NotEmpty()
+             .Matches(StringPattern.CategoryID);
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .Matches(StringPattern.Name);           
+        }
+    }
+}
