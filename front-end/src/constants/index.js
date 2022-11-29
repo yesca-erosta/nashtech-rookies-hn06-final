bad23a68cb6f6b7e338cc604dff020b841f239b8
taipt <phamtientai0311@gmail.com>
2022-11-29 13:04:41 +0700

    no message

--- front-end/src/constants/index.js
+++ front-end/src/constants/index.js
@@ -1,4 +1,3 @@
 export const BASE_URL = 'https://nashtech-rookies-hn06-gr06-api.azurewebsites.net/api';
 export const TOKEN_KEY = 'accessToken';
 export const USER_INFORMATION = 'userInformation';
-export const USER = 'User';
