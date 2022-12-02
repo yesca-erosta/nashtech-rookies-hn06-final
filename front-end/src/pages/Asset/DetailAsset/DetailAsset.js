48f25f98909ef52d149301354f7b1b2e16d7ed7f
taipt <phamtientai0311@gmail.com>
2022-12-02 10:17:55 +0700

    fix bug asset

--- front-end/src/pages/Asset/DetailAsset/DetailAsset.js
+++ front-end/src/pages/Asset/DetailAsset/DetailAsset.js
@@ -4,67 +4,79 @@ import { dateStrToStr } from '../../../lib/helper';
 import styles from '../asset.module.scss';
 
 export const DetailAsset = ({ showDetail, assetDetail, handleCloseDetail }) => {
-  return (
-    <Modal show={showDetail} onHide={handleCloseDetail}>
-      <Modal.Header>
-        <Modal.Title className={styles.modalTitle}>Asset Information</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Row>
-          <Form.Label column="sm" lg={3}>
-            Asset Code
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={assetDetail.assetCode} readOnly disabled />
-          </Col>
-        </Row>
-        <Row className="mt-3">
-          <Form.Label column="sm" lg={3}>
-            Asset Name
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={assetDetail.assetName} readOnly disabled />
-          </Col>
-        </Row>
-        <Row className="mt-3">
-          <Form.Label column="sm" lg={3}>
-            Category
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={assetDetail.category?.name} readOnly disabled />
-          </Col>
-        </Row>
-        <Row className="mt-3">
-          <Form.Label column="sm" lg={3}>
-            Installed Date
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={dateStrToStr(assetDetail.installedDate)} readOnly disabled />
-          </Col>
-        </Row>
-        <Row className="mt-3">
-          <Form.Label column="sm" lg={3}>
-            Specification
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={assetDetail.specification} readOnly disabled />
-          </Col>
-        </Row>
+    return (
+        <Modal show={showDetail} onHide={handleCloseDetail}>
+            <Modal.Header>
+                <Modal.Title className={styles.modalTitle}>Asset Information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <Form.Label column="sm" lg={3}>
+                        Asset Code
+                    </Form.Label>
+                    <Col>
+                        <Form.Control size="sm" type="text" placeholder={assetDetail.assetCode} readOnly disabled />
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Form.Label column="sm" lg={3}>
+                        Asset Name
+                    </Form.Label>
+                    <Col>
+                        <Form.Control size="sm" type="text" placeholder={assetDetail.assetName} readOnly disabled />
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Form.Label column="sm" lg={3}>
+                        Category
+                    </Form.Label>
+                    <Col>
+                        <Form.Control size="sm" type="text" placeholder={assetDetail.category?.name} readOnly disabled />
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Form.Label column="sm" lg={3}>
+                        Installed Date
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            size="sm"
+                            type="text"
+                            placeholder={dateStrToStr(assetDetail.installedDate)}
+                            readOnly
+                            disabled
+                        />
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Form.Label column="sm" lg={3}>
+                        Specification
+                    </Form.Label>
+                    <Col>
+                        <Form.Control size="sm" type="text" placeholder={assetDetail.specification} readOnly disabled />
+                    </Col>
+                </Row>
 
-        <Row className="mt-3">
-          <Form.Label column="sm" lg={3}>
-            State
-          </Form.Label>
-          <Col>
-            <Form.Control size="sm" type="text" placeholder={convertStatetoStr(assetDetail.state)} readOnly disabled />
-          </Col>
-        </Row>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="light" size="sm" className={styles.btnCancel} onClick={handleCloseDetail}>
-          Cancel
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+                <Row className="mt-3">
+                    <Form.Label column="sm" lg={3}>
+                        State
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            size="sm"
+                            type="text"
+                            placeholder={convertStatetoStr(assetDetail.state)}
+                            readOnly
+                            disabled
+                        />
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-secondary" size="sm" className={styles.btnCancel} onClick={handleCloseDetail}>
+                    Cancel
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
