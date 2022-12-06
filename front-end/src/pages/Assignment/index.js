ad81c2427dd898cad6592687dd48dbe927b21c47
nvhoan <nvhoan911@gmail.com>
2022-12-06 21:24:07 +0700

    update view assignment

--- front-end/src/pages/Assignment/index.js
+++ front-end/src/pages/Assignment/index.js
@@ -10,6 +10,8 @@ import DataTable from 'react-data-table-component';
 import ReactPaginate from 'react-paginate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faRemove } from '@fortawesome/free-solid-svg-icons';
+import { getAllDataWithFilterBox } from '../../apiServices';
+import { dateStrToStr, queryToStringForAssignments } from '../../lib/helper';
 
 const cx = classNames.bind(styles);
 
@@ -20,6 +22,84 @@ function Assignment() {
     const [placeholderState, setPlaceholderState] = useState('State');
 
     let navigate = useNavigate();
+
+    const columns = [
+        {
+            name: 'No.',
+            selector: (row, index) => index,
+            sortable: true,
+        },
+        {
+            name: 'Asset Code',
+            selector: (row) => row.assetCode,
+            sortable: true,
+        },
+        {
+            name: 'Asset Name',
+            sortable: true,
+            selector: (row) => row.assetName,
+        },
+        {
+            name: 'Assigned to',
+            selector: (row) => row.assignedTo,
+            sortable: true,
+        },
+        {
+            name: 'Assigned by',
+            selector: (row) => row.assignedBy,
+            sortable: true,
+        },
+        {
+            name: 'Assigned Date',
+            selector: (row) => row.assignedDate,
+            sortable: true,
+            cell: (row) => {
+                return <div>{dateStrToStr(row.assignedDate)}</div>;
+            },
+        },
+        {
+            name: 'State',
+            selector: (row) => row.state,
+            sortable: true,
+        },
+        {
+            name: 'Action',
+            selector: (row) => row.null,
+            cell: (row) => [
+                <Link
+                    to={`./editasset`}
+                    key={row.id}
+                    state={{ asset: row }}
+                    className={styles.customPen}
+                    style={row.state === 4 ? { cursor: 'default', color: '#b7b7b7', fontSize: '13px' } : {}}
+                >
+                    <FontAwesomeIcon icon={faPen} />
+                </Link>,
+                <Link
+                    key={`keyDelete_${row.id}`}
+                    to={'#'}
+                    style={
+                        row.state === 4
+                            ? { cursor: 'default', color: '#b7b7b7', fontSize: '1.5em', marginLeft: '10px' }
+                            : { cursor: 'pointer', color: 'red', fontSize: '1.5em', marginLeft: '10px' }
+                    }
+                >
+                    <FontAwesomeIcon icon={faRemove} />
+                </Link>,
+                <Link
+                    key={`keyDelete_${row.id}`}
+                    to={'#'}
+                    style={
+                        row.state === 4
+                            ? { cursor: 'default', color: '#b7b7b7', fontSize: '1.5em', marginLeft: '10px' }
+                            : { cursor: 'pointer', color: 'red', fontSize: '1.5em', marginLeft: '10px' }
+                    }
+                >
+                    <FontAwesomeIcon icon={faRemove} />
+                </Link>,
+            ],
+        },
+    ];
 
     useEffect(() => {
         const checkIfClickedOutside = (e) => {
@@ -65,82 +145,77 @@ function Assignment() {
         setShowState(false);
     };
 
-    const columns = [
-        {
-            name: 'No.',
-            selector: (row) => row.assetCode,
-            sortable: true,
-        },
-        {
-            name: 'Asset Code',
-            selector: (row) => row.assetName,
-            sortable: true,
-        },
-        {
-            name: 'Asset Name',
-            sortable: true,
-            selector: (row) => row.category?.name,
-        },
-        {
-            name: 'Assigned to',
-            selector: (row) => row.state,
-            sortable: true,
-        },
-        {
-            name: 'Assigned by',
-            selector: (row) => row.state,
-            sortable: true,
-        },
-        {
-            name: 'Assigned Date',
-            selector: (row) => row.state,
-            sortable: true,
-        },
-        {
-            name: 'State',
-            selector: (row) => row.state,
-            sortable: true,
-        },
+    const [loading, setLoading] = useState(false);
 
-        {
-            name: 'Action',
-            selector: (row) => row.null,
-            cell: (row) => [
-                <Link
-                    to={`./editasset`}
-                    key={row.assetCode}
-                    state={{ asset: row }}
-                    className={styles.customPen}
-                    style={row.state === 4 ? { cursor: 'default', color: '#b7b7b7', fontSize: '13px' } : {}}
-                >
-                    <FontAwesomeIcon icon={faPen} />
-                </Link>,
-                <Link
-                    key={`keyDelete_${row.assetCode}`}
-                    to={'#'}
-                    style={
-                        row.state === 4
-                            ? { cursor: 'default', color: '#b7b7b7', fontSize: '1.5em', marginLeft: '10px' }
-                            : { cursor: 'pointer', color: 'red', fontSize: '1.5em', marginLeft: '10px' }
-                    }
-                >
-                    <FontAwesomeIcon icon={faRemove} />
-                </Link>,
-            ],
-        },
-    ];
+    const [dataAssignments, setDataAssignments] = useState([]);
+    const [queryParams, setQueryParams] = useState({
+        page: 1,
+        pageSize: 10,
+        sort: 'AssignmentIdAcsending',
+        states: '0,1',
+    });
+
+    // Get Data
+    const getData = async () => {
+        setLoading(true);
+        const data = await getAllDataWithFilterBox(`Assignment/query` + queryToStringForAssignments(queryParams));
+        setDataAssignments(data.source);
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getData();
+        // I want call a function when first render
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const [totalPage, setTotalPage] = useState();
+
+    const fetchAssets = async (page) => {
+        setLoading(true);
+
+        setQueryParams({ ...queryParams, page: page, pageSize: 10 });
+
+        const data = await getAllDataWithFilterBox(`Assignment/query` + queryToStringForAssignments(queryParams));
+
+        setDataAssignments(data.source);
+        setTotalPage(data.totalRecord);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchAssets(1); // fetch page 1 of Assets
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    console.log('dataAssignments', dataAssignments);
+
+    const [selectedPage, setSelectedPage] = useState(1);
+
+    const handlePageClick = async (event) => {
+        setLoading(true);
+        setSelectedPage(event.selected + 1);
+        setQueryParams({ ...queryParams, page: event.selected + 1, pageSize: 10 });
+
+        const data = await getAllDataWithFilterBox(`Assignment/query` + queryToStringForAssignments(queryParams));
+
+        setTotalPage(data.totalRecord);
+        setDataAssignments(data.source);
+        setLoading(false);
+    };
 
     const CustomPagination = (e) => {
-        // const count = Math.ceil(totalPageHoan / perPage);
+        const count = Math.ceil(totalPage / 10);
         return (
             <Row className="mx-0">
                 <Col className="d-flex justify-content-end" sm="12">
                     <ReactPaginate
                         previousLabel={'Previous'}
                         nextLabel={'Next'}
-                        // forcePage={selectedPage !== 0 ? selectedPage - 1 : 0}
-                        // onPageChange={handlePageClick}
-                        // pageCount={count || 1}
+                        forcePage={selectedPage !== 0 ? selectedPage - 1 : 0}
+                        onPageChange={handlePageClick}
+                        pageCount={count || 1}
                         breakLabel={'...'}
                         pageRangeDisplayed={2}
                         marginPagesDisplayed={2}
@@ -166,7 +241,7 @@ function Assignment() {
                 <h1>Assignment List</h1>
             </div>
             <div className={cx('filterbox')}>
-                <div>
+                <div style={{ position: 'relative' }}>
                     <InputGroup>
                         <Form.Control placeholder={placeholderState} />
 
@@ -176,6 +251,44 @@ function Assignment() {
                             </button>
                         </InputGroup.Text>
                     </InputGroup>
+
+                    {showState && (
+                        <div className={cx('dropdown')} ref={ref}>
+                            <div className={cx('dropdown_container')}>
+                                <div className={cx('dropdown_title')}>Select type(s)</div>
+                                <div>
+                                    <Form.Check
+                                        type={'checkbox'}
+                                        label={`Accepted`}
+                                        id={`accepted`}
+                                        onChange={(e) => handleChangeCheckboxState(e, 'accepted')}
+                                        checked={checkedState.accepted}
+                                    />
+                                    <Form.Check
+                                        type={'checkbox'}
+                                        label={`Waiting for aceepted`}
+                                        id={`waitingForAccepted`}
+                                        onChange={(e) => handleChangeCheckboxState(e, 'waitingForAccepted')}
+                                        checked={checkedState.waitingForAccepted}
+                                    />
+                                </div>
+
+                                <div className={cx('button')}>
+                                    <Button variant="danger" size="sm" className={cx('button_ok')} onClick={handleOkState}>
+                                        OK
+                                    </Button>
+                                    <Button
+                                        variant="outline-secondary"
+                                        size="sm"
+                                        className={cx('button_cancel')}
+                                        onClick={handleCancelState}
+                                    >
+                                        Cancel
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
                 <div>
@@ -193,7 +306,6 @@ function Assignment() {
                 <div>
                     <InputGroup>
                         <Form.Control />
-
                         <InputGroup.Text>
                             <button className={cx('input')}>
                                 <BsSearch />
@@ -206,60 +318,22 @@ function Assignment() {
                     Create new assignment
                 </Button>
             </div>
-            {showState && (
-                <div className={cx('dropdown')} ref={ref}>
-                    <div className={cx('dropdown_container')}>
-                        <div className={cx('dropdown_title')}>Select type(s)</div>
-                        <div>
-                            <Form.Check
-                                type={'checkbox'}
-                                label={`Accepted`}
-                                id={`accepted`}
-                                onChange={(e) => handleChangeCheckboxState(e, 'accepted')}
-                                checked={checkedState.accepted}
-                            />
-                            <Form.Check
-                                type={'checkbox'}
-                                label={`Waiting for aceepted`}
-                                id={`waitingForAccepted`}
-                                onChange={(e) => handleChangeCheckboxState(e, 'waitingForAccepted')}
-                                checked={checkedState.waitingForAccepted}
-                            />
-                        </div>
-
-                        <div className={cx('button')}>
-                            <Button variant="danger" size="sm" className={cx('button_ok')} onClick={handleOkState}>
-                                OK
-                            </Button>
-                            <Button
-                                variant="outline-secondary"
-                                size="sm"
-                                className={cx('button_cancel')}
-                                onClick={handleCancelState}
-                            >
-                                Cancel
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            )}
 
             <div className={cx('main_table')}>
                 <DataTable
                     title="Assets"
                     columns={columns}
-                    // data={assetsHoan}
+                    data={dataAssignments}
                     noHeader
-                    defaultSortField="id"
                     defaultSortAsc={true}
                     highlightOnHover
-                    noDataComponent={'There are no records to display'}
+                    // noDataComponent={'There are no records to display'}
                     dense
-                    // progressPending={loading}
+                    progressPending={loading}
                     pagination
                     paginationComponent={CustomPagination}
                     paginationServer
-                    sortServer
+                    // sortServer
                     // onSort={handleSort}
                 />
             </div>
