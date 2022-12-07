00dec45758a1546f881024e41961b3f9b1d87558
nvhoan <nvhoan911@gmail.com>
2022-12-07 13:15:09 +0700

    update assignment

--- front-end/src/pages/Asset/index.js
+++ front-end/src/pages/Asset/index.js
@@ -14,6 +14,7 @@ import { deleteData, getAllData, getAllDataWithFilterBox, getOneData } from '../
 import { ASSET } from '../../constants';
 import { queryToStringForAsset } from '../../lib/helper';
 import { DetailAsset } from './DetailAsset/DetailAsset';
+import { ShowModalDelete } from './showDelete/showDelete';
 
 const cx = classNames.bind(styles);
 
@@ -414,7 +415,11 @@ function Asset() {
                     key={row.assetCode}
                     state={{ asset: row }}
                     className={styles.customPen}
-                    style={row.state === 4 ? { cursor: 'default', color: '#b7b7b7', fontSize: '13px' } : {}}
+                    style={
+                        row.state === 4
+                            ? { cursor: 'default', color: '#b7b7b7', fontSize: '13px' }
+                            : { color: 'rgb(102, 101, 101)' }
+                    }
                 >
                     <FontAwesomeIcon icon={faPen} />
                 </Link>,
@@ -741,28 +746,7 @@ function Asset() {
                 msgNoData()
             )}
             <DetailAsset showDetail={showDetail} assetDetail={assetDetail} handleCloseDetail={handleCloseDetail} />
-
-            {/* TODO: handle modal delete */}
-            {/* <ModalDelete showDetail={showDetail} assetDetail={assetDetail} handleCloseDetail={handleCloseDetail} /> */}
-
-            <Modal show={showDelete} onHide={() => setShowDelete(false)}>
-                <Modal.Header>
-                    <Modal.Title>Are you sure?</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Label>Do you want to delete this asset?</Form.Label>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="danger" onClick={handleDelete}>
-                        Delete
-                    </Button>
-                    <Button variant="outline-secondary" onClick={() => setShowDelete(false)}>
-                        Cancel
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            <ShowModalDelete showDelete={showDelete} setShowDelete={setShowDelete} handleDelete={handleDelete} />
 
             <Modal
                 show={isShowModalCantDelete}
---
+++ front-end/src/pages/Assignment/EditAssignment/EditAssignment.js
@@ -0,0 +1,82 @@
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import { InputGroup } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { BsSearch } from 'react-icons/bs';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ModalAsset } from '../Modal/ModalAsset/ModalAsset';
+import { ModalUser } from '../Modal/ModalUser/ModalUser';
+import styles from './editAssignment.module.scss';
+
+const cx = classNames.bind(styles);
+
+function EditAssignment() {
+    const navigate = useNavigate();
+    const [isShowListUser, setIsShowListUser] = useState(false);
+    const [isShowListAsset, setIsShowListAsset] = useState(false);
+    const [nameAsset, setNameAsset] = useState();
+    const [nameUser, setNameUser] = useState();
+
+    const location = useLocation();
+    const { assignment } = location?.state;
+    console.log(assignment);
+
+    return (
+        <div className={cx('container')}>
+            <h3 className={cx('title')}>Edit Assignment</h3>
+
+            <Form className={cx('form')}>
+                <Form.Group className={cx('common-form')}>
+                    <Form.Label className={cx('title_input')}>User</Form.Label>
+                    <InputGroup>
+                        <Form.Control placeholder={'Enter user'} style={{ width: 600 }} readOnly value={nameUser} />
+                        <InputGroup.Text style={{ cursor: 'pointer' }} onClick={() => setIsShowListUser(true)}>
+                            <BsSearch />
+                        </InputGroup.Text>
+                    </InputGroup>
+                </Form.Group>
+
+                <Form.Group className={cx('common-form')}>
+                    <Form.Label className={cx('title_input')}>Asset</Form.Label>
+                    <InputGroup>
+                        <Form.Control placeholder={'Enter asset'} readOnly value={nameAsset} />
+                        <InputGroup.Text style={{ cursor: 'pointer' }} onClick={() => setIsShowListAsset(true)}>
+                            <BsSearch />
+                        </InputGroup.Text>
+                    </InputGroup>
+                </Form.Group>
+
+                <Form.Group className={cx('common-form')}>
+                    <Form.Label className={cx('title_input')}>Assigned Date</Form.Label>
+                    <Form.Control type="date" />
+                </Form.Group>
+
+                <Form.Group className={cx('common-form')}>
+                    <Form.Label className={cx('title_input')}>Note</Form.Label>
+                    <Form.Group className="w-100">
+                        <Form.Control type="text" as="textarea" rows={5} cols={40} placeholder="Enter note" />
+                    </Form.Group>
+                </Form.Group>
+
+                <div className={cx('button')}>
+                    <Button variant="danger">Save</Button>
+
+                    <Button
+                        variant="outline-secondary"
+                        className={cx('cancel-button')}
+                        onClick={() => navigate('/manageassignment')}
+                    >
+                        Cancel
+                    </Button>
+                </div>
+            </Form>
+
+            {isShowListUser && <ModalUser setIsShowListUser={setIsShowListUser} setNameUser={setNameUser} />}
+
+            {isShowListAsset && <ModalAsset setIsShowListAsset={setIsShowListAsset} setAsset={setNameAsset} />}
+        </div>
+    );
+}
+
+export default EditAssignment;
