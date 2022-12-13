21dbf76cab6764f78b82f6b72f9058a52e5ff7ab
DESKTOP-2J9NP80\PhungVu <ducbh88@gmail.com>
2022-12-13 12:08:55 +0700

    Export excel sample

---
+++ back-end/AssetManagementTeam6.API/Reports/ExportExcel.GenerateColumns.cs
@@ -0,0 +1,32 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace AssetManagementTeam6.API.Reports
+{
+    public partial class ExportExcel
+    {
+        public Columns GenerateColumnsForSampleReport()
+        {
+            Columns columnSet = new();
+
+            Column cc;
+            cc = new Column() { Min = 1U, Max = 1U, Width = 15D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 2U, Max = 2U, Width = 28D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 3U, Max = 3U, Width = 15D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 4U, Max = 4U, Width = 20D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 5U, Max = 5U, Width = 15D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 6U, Max = 6U, Width = 50D, CustomWidth = true };
+            columnSet.Append(cc);
+            cc = new Column() { Min = 7U, Max = 7U, Width = 25D, CustomWidth = true };
+            columnSet.Append(cc);
+
+            return columnSet;
+        }
+
+        //TODO: Create other GenerateColumn Function Here
+    }
+}
